812e57acc8fdafec25cd5301422ec61015e2ac22
zeinline <71123844+zeinline@users.noreply.github.com>
2021-09-29 19:15:55 +0600

    up

--- web_data/info.js
+++ web_data/info.js
@@ -1,14 +1,14 @@
-var NinjaRunner = {
-  way: 'F:/OpenServer/domains/zein_line/games/html%20game/ninja%20runner/',
+var Puzzle = {
+  way: 'https://zeinline.github.io/Game.Puzzle/',
   
-  title: ['Ninja Runner', 'Ниндзя Бегун'],
-  size: 18,
-  vers: '1.0.0',
+  title: ['Puzzle', 'Пазл'],
+  size: 1240,
+  vers: '0.1.0',
   date: [4, 7, 21, 9, 7, 21],
   brief: ['', 'пока нет описания'],
   desc: ['', 'пока нет описания'],
 
   tag_build: 'web',
   tag_player: 1,
-  tag: ['demo', 'ninja', 'runner'],
+  tag: ['demo', 'puzzle'],
 }
